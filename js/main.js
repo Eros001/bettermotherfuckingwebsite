5df5cabea0e2bce93a05e21ef25190f96f7d749f
Eros <shurii@hotmail.fr>
2018-11-18 21:13:51 +0100

    V3 DEBUG

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function MonFichierPopUp () {
+	var confirme= confirm('Veut tu aller sur mon site porno xxxvidsxxx ?, Clique sur <OK>');
+	if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") ; }
+	else { alert('Dommage...'); } }
+setTimeout(function(){MonFichierPopUp() },10000)
+
